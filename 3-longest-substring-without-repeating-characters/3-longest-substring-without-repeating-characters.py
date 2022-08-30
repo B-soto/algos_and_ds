a8b40fcc5cddc8d850307c308044da077b41f345
Bryce Soto <brycesoto@gmail.com>
2022-08-30 15:11:08 -0700

    Time: 63 ms (74.30%), Space: 13.9 MB (54.35%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -4,12 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        chars = []
-        maxLength = 0
-        for i in range(len(s)):
-            while s[i] in chars:
-                del chars[0]
-            chars.append(s[i])
-            maxLength = max(len(chars), maxLength)
+        max_ss = 0
+        seen_set = set()
+        sliding_window = []
+
+        for char in s:
+            while char in seen_set:
+                seen_set.remove(sliding_window[0])
+                del sliding_window[0]
+
+
+            sliding_window.append(char)
+            seen_set.add(char)
+            max_ss = max(max_ss, len(sliding_window))
+      
+  
+        return max_ss
+#         chars = []
+#         maxLength = 0
+#         for i in range(len(s)):
+#             while s[i] in chars:
+#                 del chars[0]
+#             chars.append(s[i])
+#             maxLength = max(len(chars), maxLength)
             
-        return maxLength
+#         return maxLength
