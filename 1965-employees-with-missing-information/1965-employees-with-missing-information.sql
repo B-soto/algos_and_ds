ecde68e021b416d38b0d0cad20a815e006a0243d
Bryce Soto <brycesoto@gmail.com>
2022-09-09 18:31:00 -0700

    Time: 493 ms (81.32%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+(SELECT Employees.employee_id FROM Employees
+LEFT JOIN Salaries 
+ON Employees.employee_id = Salaries.employee_id
+where salary is null)
+
+UNION 
+
+(SELECT Salaries.employee_id FROM Salaries 
+LEFT JOIN Employees
+ON Salaries.employee_id = Employees.employee_id
+WHERE name is null )
+ORDER BY employee_id
+
