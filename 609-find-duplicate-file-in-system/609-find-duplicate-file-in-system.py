60b7a0d623b5276bed29d3a579081e228e2e7466
Bryce Soto <brycesoto@gmail.com>
2022-09-19 14:56:27 -0700

    Time: 156 ms (49.24%), Space: 23.5 MB (87.52%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
@@ -1,20 +1,15 @@
 class Solution:
-    def get_dups(self, my_dict):
-        
-        
-        return 
-        
-                
+  
     def findDuplicate(self, paths: List[str]) -> List[List[str]]:
         seen_dict = {}# word:file
         duplicate_list = []
         
         for path in paths:
-            # print(path)
-            # print(path.split(' '))
+           
             path_arr = path.split(' ')
             path_dir = path_arr[0]
             path_files = path_arr[1:]
+            
             for file in path_files:
                 text = []
                 file_name = []
@@ -29,14 +24,11 @@ class Solution:
                     
                 full_file_path = path_dir + '/' + ''.join(file_name)
                 text = ''.join(text).strip('()')
-                # print(full_file_path , "-", text)
+                
                 if text not in seen_dict:
                     seen_dict[text] = [full_file_path]
                 else:
                     seen_dict[text].append(full_file_path)
-            
-        # print(seen_dict)
 
-        # answer = self.get_dups(seen_dict)
         return [v for k,v in seen_dict.items() if len(v)>1]
         
