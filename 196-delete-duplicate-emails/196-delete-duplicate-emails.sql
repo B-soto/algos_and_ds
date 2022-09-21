32300d2a5ba5f23e21736f2f3ea31b8a05c995e7
Bryce Soto <brycesoto@gmail.com>
2022-09-20 18:15:23 -0700

    Time: 1332 ms (16.73%), Space: 0B (100.00%) - LeetHub

---
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -0,0 +1,8 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+DELETE p1.*
+FROM Person as p1, Person as p2
+WHERE
+    p1.Email = p2.Email
+and 
+    p1.id > p2.id;
