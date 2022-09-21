c861b7d17ca18d88eb94c3434c88bf3943cd4a59
Bryce Soto <brycesoto@gmail.com>
2022-09-21 12:45:52 -0700

    Time: 997 ms (31.54%), Space: 20.5 MB (75.57%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
@@ -9,11 +9,9 @@ class Solution:
             i = q[1]
             
             if val%2 != 0 and nums[i]%2 != 0:
-                # print(val)
                 nums[i] += val
-                # print(nums)
                 current_even_count += nums[i]
-                # print(current_even_count)
+                
             
             elif val%2 == 0 and nums[i]%2 == 0:
                 current_even_count += val
@@ -23,7 +21,7 @@ class Solution:
                 current_even_count -= nums[i]
                 nums[i] += val
                 
-            elif val%2 == 0 and nums[i]%2 != 0:
+            else:# val%2 == 0 and nums[i]%2 != 0:
                 nums[i] += val
                 
             return_arr.append(current_even_count)
