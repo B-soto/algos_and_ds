3c7eb980bcac73bb966876f21e6751308d407cdc
Bryce Soto <brycesoto@gmail.com>
2022-09-20 21:32:14 -0700

    Time: 332 ms (40.71%), Space: 0B (100.00%) - LeetHub

--- 627-swap-salary/627-swap-salary.sql
+++ 627-swap-salary/627-swap-salary.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
+# UPDATE Salary
+# SET sex = IF(sex = 'm', 'f', 'm')
+
 UPDATE Salary
-SET sex = IF(sex = 'm', 'f', 'm')
+SET sex =
+CASE
+    WHEN sex = 'm' THEN 'f'
+    ELSE 'm'
+END;
     
 
 
