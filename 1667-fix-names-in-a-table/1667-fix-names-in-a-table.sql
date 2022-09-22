4f8d051096ae0ef840a6f64bc5a8059ae3986efc
Bryce Soto <brycesoto@gmail.com>
2022-09-22 00:36:51 -0700

    Time: 743 ms (56.66%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,5 +1,22 @@
-# Write your MySQL query statement below
-SELECT user_id, 
-CONCAT(UPPER(SUBSTR(name,1,1)), LOWER(SUBSTR(name,2))) AS name 
-FROM users
+
+
+SELECT user_id,
+CONCAT(UPPER(SUBSTR(name, 1,1)), LOWER(SUBSTR(name, 2))) as name
+FROM Users
 ORDER BY user_id;
+
+
+
+
+
+
+
+
+
+
+
+# Write your MySQL query statement below
+# SELECT user_id, 
+# CONCAT(UPPER(SUBSTR(name,1,1)), LOWER(SUBSTR(name,2))) AS name 
+# FROM users
+# ORDER BY user_id;
