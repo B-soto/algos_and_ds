675dfef45b40e58c675fa3b61267c361409e5f7f
Bryce Soto <brycesoto@gmail.com>
2023-04-29 15:47:01 -0700

    Time: 128 ms (5.92%), Space: 30.2 MB (5.24%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,8 +1,6 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        # O(n)
-        # O(n2)
-        print(len(s))
+       
         sequences_seen = {}
         
         left = 0
@@ -11,8 +9,7 @@ class Solution:
         #get freq of all possible 10 strand dna 
         while right < len(s):
             sub_string = ''.join(s[left:right+1])
-            # print(sub_string)
-
+            
             
             if sub_string in sequences_seen:
                 sequences_seen[sub_string] +=1
@@ -22,7 +19,7 @@ class Solution:
             left +=1
             right +=1
                 
-        # print([k for k,v in sequences_seen.items() if v > 1])
+        
         return [k for k,v in sequences_seen.items() if v > 1]
     
     
