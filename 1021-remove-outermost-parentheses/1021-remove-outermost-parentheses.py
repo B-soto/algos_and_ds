df0513e8c9f3a3f7c792298714cedeb8137867ce
Bryce Soto <brycesoto@gmail.com>
2023-05-04 00:44:03 -0700

    Time: 58 ms (7.36%), Space: 16.4 MB (8.17%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,30 +1,6 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
         
-        """
-
-        return string or list
-
-        curr_string
-        iterate over string s
-        when we see a (, throw it in our stack
-        while stack, 
-            we should addd to current string whenever we see a ). pop the top of stack and append the pair to curr 
-        once stack is done, we add curr string to return_list and reset curr_string
-
-
-
-        (()())(())(()(()))
-                         
-        stack = {
-            
-            
-            (
-        }
-        count = 1
-        curr_string = ()()
-        return = []
-        """
         return_list = []
         count = 0
 
