fed282460352b6d3bc67cb18463f79e5433a7923
Bryce Soto <brycesoto@gmail.com>
2023-05-07 00:25:28 -0700

    Time: 43 ms (11.83%), Space: 16.2 MB (15.10%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,22 +1,32 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cache = {}
-        def dfs(s):
-            
-            if s in cache:
-                return cache[s]
-            if s < 0:
-                return 0 
-            if s == 0 :
-                return 1
-            
-            left = dfs(s -1)
-            right = dfs(s -2)
-            
-            cache[s] = left+right
-            
-            return left + right
+        if n ==1:
+            return n
+        table = [0 for x in range(n)]
+        table[0]= 1
+        table[1]= 2
         
-        total_ways = dfs(n)
-        return total_ways
+        for i in range(2,len(table)):
+            table[i] = table[i-1]+ table [i-2]
+        return table [-1]
+        
+#         cache = {}
+#         def dfs(s):
+            
+#             if s in cache:
+#                 return cache[s]
+#             if s < 0:
+#                 return 0 
+#             if s == 0 :
+#                 return 1
+            
+#             left = dfs(s -1)
+#             right = dfs(s -2)
+            
+#             cache[s] = left+right
+            
+#             return left + right
+        
+#         total_ways = dfs(n)
+#         return total_ways
                 
