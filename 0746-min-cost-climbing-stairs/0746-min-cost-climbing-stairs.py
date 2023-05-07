76b067de5798dd484048f41370ec8b2d2d1480b6
Bryce Soto <brycesoto@gmail.com>
2023-05-06 23:34:47 -0700

    Time: 64 ms (42.81%), Space: 16.4 MB (11.27%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,29 +1,34 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        cache = {}
+        cost.append(0)
+        for i in range(2,len(cost)):
+            cost[i] = cost[i] + min(cost[i-1], cost[i-2])
+        return cost[-1]
+            
+#         n = len(cost)
+#         cache = {}
        
-        def minCost(i):
+#         def minCost(i):
             
-            if i in cache:
-                return cache[i]
+#             if i in cache:
+#                 return cache[i]
             
-            if i < 0:
-                return 0
-            if i == 0 or i == 1:
-                return cost[i]
+#             if i < 0:
+#                 return 0
+#             if i == 0 or i == 1:
+#                 return cost[i]
             
-            left = minCost(i-1)
-            right = minCost(i-2)
+#             left = minCost(i-1)
+#             right = minCost(i-2)
             
-            total = cost[i] + min(left,right)
+#             total = cost[i] + min(left,right)
             
             
-            cache[i] = total
-            return total
+#             cache[i] = total
+#             return total
             
         
-        left = minCost(n-1)
-        right = minCost(n-2)
+#         left = minCost(n-1)
+#         right = minCost(n-2)
         
-        return min(left,right)
+#         return min(left,right)
