1225a008499b77fa271d02aa342eda6907673f5a
Bryce Soto <brycesoto@gmail.com>
2023-05-06 21:16:08 -0700

    Time: 147 ms (76.74%), Space: 16.5 MB (51.45%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,45 +1,70 @@
 class Solution:
     def change(self, amount: int, AllCoins: List[int]) -> int:
+        grid = [0 for _ in range(amount +1)]
+        grid[0] = 1
+        print(grid)
         
+        for coin in AllCoins:
+            for i in range(coin, len(grid)):
+                grid[i] = grid[i] + grid[i-coin]
+        print(grid)
+        return grid[-1]
+        
+#           0. 1. 2. 3. 4. 5
+#         |----------------------
+#     []  | 1  0. 0. 0  0. 0
+#     [1] | 1. 1. 1. 1. 1. 1
+#    [1,2]| 1  1  2  2  3  3 
+#  [1,2,5]|                4
+        
+    #forumla = up row, + row - new_coin value
+    
         
 #                                 5 [1,2,5]
-#                                 /.    \
-#                         0[1,2,5]      5[1,2]
-#                       /      \
-#             -5[1,2,5]       0[1,2]
-#                                \ 
-#                             0[1]
-#                                 \ 
-#                          -1[0]      0[]
-        cache = {}
-        def recurse(amount, AllCoins) -> int:
-            key = str(amount) + "_" + str(AllCoins)
+#                                 /.         \
+#                         0[1,2,5]              5[1,2]
+#                                                  /      \ 
+#                       /      \                3[1,2]   5[1]
+#             -5[1,2,5]       0[1,2]             /   \
+#                                \           1[1,2]    3[1]
+#                             0[1]            /\         /\
+#                                 \       -1[1]  1[1]  2[1] \3[]
+# #                          -1[0]      0[]                /\
+#                                                      1[1]    2[]
+#                                                         /\
+#                                                     0[1]  1[]
+#                                                       \
+#                                                         0[]
+    
+#         cache = {}
+#         def recurse(amount, AllCoins) -> int:
+#             key = str(amount) + "_" + str(AllCoins)
             
-            if key in cache:
-                return cache[key]
-            if amount == 0 and len(AllCoins) == 0:                
-                return 1
-            if len(AllCoins) < 1:
-                return 0
-            if amount < 0:
-                return 0 
-            
-            
-            #recursive Case
-            left = recurse(amount - AllCoins[-1], AllCoins)
-            
-            last_coin = AllCoins.pop()
-            right = recurse(amount, AllCoins)
-            AllCoins.append(last_coin)
-            
+#             if key in cache:
+#                 return cache[key]
+#             if amount == 0 and len(AllCoins) == 0:                
+#                 return 1
+#             if len(AllCoins) < 1:
+#                 return 0
+#             if amount < 0:
+#                 return 0 
             
             
-            cache[key] = right + left
-            return right + left
+#             #recursive Case
+#             left = recurse(amount - AllCoins[-1], AllCoins)
+            
+#             last_coin = AllCoins.pop()
+#             right = recurse(amount, AllCoins)
+#             AllCoins.append(last_coin)
+            
+            
+            
+#             cache[key] = right + left
+#             return right + left
             
         
         
         
-        possible_ways = recurse(amount, AllCoins)
-        return possible_ways
+#         possible_ways = recurse(amount, AllCoins)
+#         return possible_ways
         
