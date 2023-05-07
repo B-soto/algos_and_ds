ca054be738853e1b152ec481a0feec9b90542e50
Bryce Soto <brycesoto@gmail.com>
2023-05-07 00:26:53 -0700

    Time: 52 ms (5.44%), Space: 16.1 MB (15.10%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -6,7 +6,7 @@ class Solution:
         table[0]= 1
         table[1]= 2
         
-        for i in range(2,len(table)):
+        for i in range(2,n):
             table[i] = table[i-1]+ table [i-2]
         return table [-1]
         
