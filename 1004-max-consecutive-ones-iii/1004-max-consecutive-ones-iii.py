8ef4322a6833b15d99106aa823d1b6d278ea0453
Bryce Soto <brycesoto@gmail.com>
2023-05-18 01:08:45 -0700

    Time: 666 ms (14.91%), Space: 17.1 MB (13.70%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -12,9 +12,7 @@ class Solution:
         
         
         '''
-        # [1,1,1,0,0,0,1,1,1,1,0]
-        #        r
-        # l
+        
         longest = 0
         flips_left = k
         l,r = 0,0
@@ -23,17 +21,13 @@ class Solution:
         while r < len(nums):
             if nums[r] == 1:
                 curr = (r+1)-l
-                print(r,l,curr)
                 longest = max(longest, curr)
                 r +=1
                 continue
                 
             if nums[r] == 0 and flips_left > 0:
                 flips_left -=1
-                curr = (r+1)-l
-                print(r,l, flips_left, curr)
-                longest = max(longest, curr)
-                r +=1
+                
             else:
                 while flips_left == 0:
                     if nums[l] == 1:
@@ -43,11 +37,12 @@ class Solution:
                         flips_left +=1
                         l +=1
                 flips_left -=1
-                curr = (r+1)-l
-                print(r,l, flips_left, curr)
-                longest = max(longest, curr)
-                r +=1
-        return(longest)
+                
+            curr = (r+1)-l
+            longest = max(longest, curr)
+            r +=1
+            
+        return longest
                 
                 
             
