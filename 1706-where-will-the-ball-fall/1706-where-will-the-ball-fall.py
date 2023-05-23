3504ed598dd28b40446dd385b37feba96b18969e
Bryce Soto <brycesoto@gmail.com>
2023-05-22 18:11:01 -0700

    Time: 209 ms (33.92%), Space: 20.8 MB (5.24%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -3,8 +3,11 @@ class Solution:
         
         rows = len(grid)
         cols = len(grid[0])
+        cache = {}
         
         def dfs(r,c):
+            if (r,c) in cache:
+                return cache[(r,c)]
             if c < 0 or c >=cols:
                 return -1
             if r == rows:
@@ -13,12 +16,16 @@ class Solution:
                 if c+1 < cols and grid[r][c+1] == -1:
                     return -1
                 else:
-                    return dfs(r+1, c+1)
+                    ans = dfs(r+1, c+1)
+                    cache[(r,c)] = ans
+                    return ans
             else:
                 if c-1 >= 0 and grid[r][c-1] == 1:
                     return -1
                 else:
-                    return dfs(r+1,c-1)
+                    ans = dfs(r+1,c-1)
+                    cache[(r,c)] = ans
+                    return ans
                 
         return [dfs(0,i) for i in range(cols)]
                     
