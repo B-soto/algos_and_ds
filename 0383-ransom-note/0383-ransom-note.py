e284fb69e19186b82ff3f936020abc20c2012804
Bryce Soto <brycesoto@gmail.com>
2023-06-01 17:23:53 -0700

    Time: 71 ms (53.02%), Space: 16.7 MB (14.82%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,10 @@
 from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        # note = {a:2} mag = {a:2 b:1}
-        note = dict(Counter(ransomNote))
-        mag = dict(Counter(magazine))
-        print(note, mag)
+        
+        note = Counter(ransomNote)
+        mag = Counter(magazine)
+
         
         for k,v in note.items():
             if k not in mag or note[k] > mag[k]:
