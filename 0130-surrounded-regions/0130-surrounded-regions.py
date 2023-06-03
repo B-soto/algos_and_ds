f1ca9b3073f01cb8fa3cf5712b7419b4d8ddb822
Bryce Soto <brycesoto@gmail.com>
2023-06-02 20:50:23 -0700

    Time: 164 ms (37.51%), Space: 17.9 MB (58.68%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -28,7 +28,7 @@ class Solution:
             if board[i][len(board[0])-1] == 'O':
                 q.append([i,len(board[0])-1])
                   
-        print(q)
+        # print(q)
         
         # Do a BFS on all in the q:
         visited = set()
@@ -48,7 +48,7 @@ class Solution:
             q.append((r,c+1))
             q.append((r,c-1))
                 
-        print(visited)
+        # print(visited)
         for i in range(len(board)):
             for j in range(len(board[0])):
                 # print(i,j)
