9b58264c8c987ebeb5f0e4a9883f85f4aff6aa26
Bryce Soto <brycesoto@gmail.com>
2023-06-03 18:30:29 -0700

    Time: 35 ms (77.07%), Space: 16.5 MB (8.45%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        
+        
+        
+        s = s[::-1]
+        s = s.strip() + ' ' 
+        
+        if len(s) == 1:
+            return 1
+        
+        rp = 0
+        for lp in range(len(s)):
+            if s[lp] == ' ':
+                break
+        print(lp,rp)
+        return(lp-rp)
+                
