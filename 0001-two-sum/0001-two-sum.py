204fb44e3eae889fc9f22500de6dc05bad607d92
Bryce Soto <brycesoto@gmail.com>
2023-06-05 11:47:51 -0700

    Time: 82 ms (48.17%), Space: 17.7 MB (22.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,26 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        # seen_set = {2:1, 3:0, }
-        
-        # create a seen dictionary
-        
-        # iterate over our nums list, subtract num from target and see if complimentry value has been seen yet. 
-        #If not add it to the dict
-        #If so return the seen values
-        
-        
-        # End of program return False
-        
-        seen_nums = {}
-        
-        for i in range(len(nums)):
-            val = nums[i]
-            comp = target - val
+            seen_set = {}
+            # [2,7,11,15]
             
-            if comp not in seen_nums:
-                seen_nums[val] = i
-            else:
-                return [seen_nums[comp], i]
+            for i in range(len(nums)):
+                num = nums[i]
+                comp_value = target - num
+                
+                if comp_value in seen_set:
+                    return [i, seen_set[comp_value]]
+                else:
+                    seen_set[num] = i
+            
+            
             
         
